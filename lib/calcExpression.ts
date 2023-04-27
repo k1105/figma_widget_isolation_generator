520d90e4d4824a87eb564adb580aea28c3eae868
k1105 <kanapon1105@yahoo.co.jp>
2023-04-27 10:17:08 +0900

    bugfix: masterのサイズに小数を含む場合に対応する

--- lib/calcExpression.ts
+++ lib/calcExpression.ts
@@ -6,14 +6,16 @@ export const calcExpression: (str: string) => string = (str: string) => {
   if (Number(str.split("*")[0])) {
     //一番最初に評価された式が掛け算だった場合
     const multiExpression = (
-      str.match(/[0-9]+\*[0-9]+/g) as RegExpMatchArray
+      str.match(/[0-9\.]+\*[0-9\.]+/g) as RegExpMatchArray
     )[0];
     const nums = multiExpression.split("*");
     const res = Number(nums[0]) * Number(nums[1]);
     return calcExpression(str.replace(multiExpression, String(res)));
   } else {
     //一番最初に評価された式が割り算だった場合
-    const divExpression = (str.match(/[0-9]+\/[0-9]+/g) as RegExpMatchArray)[0];
+    const divExpression = (
+      str.match(/[0-9\.]+\/[0-9\.]+/g) as RegExpMatchArray
+    )[0];
     const nums = divExpression.split("/");
     const res = Number(nums[0]) / Number(nums[1]);
     return calcExpression(str.replace(divExpression, String(res)));
--- lib/validateExpression.ts
+++ lib/validateExpression.ts
@@ -1,5 +1,5 @@
 export const validateExpression = (str: string) => {
-  if (!str.match(/[^0-9\*\/]/g)) {
+  if (!str.match(/[^0-9\.\*\/]/g)) {
     if (!str.split("*").includes("") && !str.split("/").includes("")) {
       return true;
     }
